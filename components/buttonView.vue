2eddb693e5b3355d6b3221b161ff80aa4232b1e7
Justus <martinvegance@live.com>
2022-09-29 01:29:44 +0800

    fix

---
+++ components/buttonView.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		<button type="default">sdasasds</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"buttonView",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
